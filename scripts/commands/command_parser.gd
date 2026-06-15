675e1053db431a3a31c3bf1f09ad53313118dab9
Luca Neukom <luca.neukom@gmail.com>
2026-06-15 18:40:32 +0200

    add new commands to parser

--- scripts/commands/command_parser.gd
+++ scripts/commands/command_parser.gd
@@ -16,10 +16,18 @@ static func parse(to_parse: String) -> Array[Command]:
 			parsed = Rotate.build(arguments)
 		"spin":
 			parsed = Spin.build(arguments)
-		"execute":
-			parsed = Execute.build(arguments)
 		"stop-spin":
 			parsed = StopSpin.build(arguments)
+		"speed":
+			parsed = Speed.build(arguments)
+		"break":
+			parsed = StopShip.build(arguments)
+		"execute":
+			parsed = Execute.build(arguments)
+		"abort":
+			parsed = Abort.build(arguments)
+		"cancel":
+			parsed = Cancel.build(arguments)
 		var unknown_command:
 			ConsoleHelper.send_message("Unknown command " + unknown_command)
 	if parsed is Array[Command]:
